2f4a01c7b7ea07c474249a3644b814be973c2034
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2020-07-13 20:06:41 +0200

    add register, but it is now stupid

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
 My first site
 <br>
 </head>
-<body>
-<?php echo "Hello" ?>
-</body>
+	<body>
+		<form action="welcome.php" method="post">
+				Name: <input type="text" name="name"><br>
+				Password: <input type="text" name="password"><br>
+			<input type="submit" value="Submit">
+		</form>
+		<form action="register.php" method="post">
+			<input type="submit" value="Click here to register">
+		</form>
+	</body>
 </html>
---
+++ welcome.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your password: <?php echo $_POST["password"]; ?><br>
+</body>
+</html>
