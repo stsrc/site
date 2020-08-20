259099eefc4e062cf456dffc54bca95c72d4ebf4
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2020-08-20 19:22:40 +0200

    Force https

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-    session_start();
+session_start();
+
+if ($_SERVER["HTTPS"] != "on") {
+	header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+	exit();
+}
 ?>
 
 <html>
