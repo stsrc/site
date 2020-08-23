43e068140f670b1d6792cd2915381bcdad1f8e8a
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2020-08-23 21:41:29 +0200

    Make register site a little prittier

--- register.php
+++ register.php
@@ -11,12 +11,18 @@
 		<link rel="stylesheet" href="register.css" type="text/css">
 	</head>
 	<body>
+	<div id="header">
+		<h1>Yet another fancy site</h1>
+		<hr>
+	</div>
 	<div id="container">
 		<div id="one">
 			<form action="register2.php" method="post">
-				Username:<br><input type="text" name="username"><br>
+				Username:<br><input type="text" name="username">
+				<p style="font-size:12px"> 1-20 characters. Allowed characters: A-Z, a-z, 0-9, !, @, #, $, %, ^, &, *, (, ),_</p><br>
 				E-mail:<br><input type="text" name="email"><br>
-				Password:<br><input type="password" name="password"><br>
+				Password:<br><input type="password" name="password">
+				<p style="font-size:12px"> 6-20 characters. Allowed characters: A-Z, a-z, 0-9, !, @, #, $, %, ^, &, *, (, ),_</p><br>
 				Retype password:<br><input type="password" name="repassword"><br><br>
 	<?php
 		echo '<img src="' . $_SESSION['captcha']['image_src'] . '" alt="CAPTCHA code"><br>';
