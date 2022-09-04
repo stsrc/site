735e6037bcbcfec2c3339ef0a4a99bac1b05bf14
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2022-09-04 15:48:12 +0200

    Reformat removenote.php

--- removenote.php
+++ removenote.php
@@ -11,21 +11,21 @@
 ?>
 
 <html>
-<body>
-<?php
-	$toremove=$_POST["notes"];
-	include 'secretpasswords.php';
-	try {
-		$pdo = new PDO("mysql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname, $user, $password);
-	} catch (PDOException $e) {
-		echo "WRONG! PDO failed";
-		throw new Exception($e->getMessage());
-	}
-	$query=$pdo->prepare("DELETE FROM blog where blog_id=?");
-	$query->execute([$toremove]);
+	<body>
+		<?php
+			$toremove=$_POST["notes"];
+			include 'secretpasswords.php';
+			try {
+				$pdo = new PDO("mysql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname, $user, $password);
+			} catch (PDOException $e) {
+				echo "WRONG! PDO failed";
+				throw new Exception($e->getMessage());
+			}
+			$query=$pdo->prepare("DELETE FROM blog where blog_id=?");
+			$query->execute([$toremove]);
 
-	$query=$pdo->prepare("DELETE FROM comments where blog_id=?");
-	$query->execute([$toremove]);
-?>
-</body>
+			$query=$pdo->prepare("DELETE FROM comments where blog_id=?");
+			$query->execute([$toremove]);
+		?>
+	</body>
 </html>
