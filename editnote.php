dcd6e5a14f7725b74e6ea15c9001602449e38d05
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2022-09-04 15:01:49 +0200

    Reformat editnote.php

--- editnote.php
+++ editnote.php
@@ -9,35 +9,35 @@
 ?>
 
 <html>
-<body>
-<?php
-	$toedit=$_POST["notes"];
-	include 'secretpasswords.php';
-	try {
-		$pdo = new PDO("mysql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname, $user, $password);
-	}  catch (PDOException $e) {
-		echo "WRONG! PDO failed";
-		throw new Exception($e->getMessage());
-	}
-	$query=$pdo->prepare("SELECT * FROM blog where blog_id=?");
-	$query->execute([$toedit]);
-	$query->setFetchMode(PDO::FETCH_NUM);
-	$row = $query->fetch();
-	$_SESSION['id'] = $row[0];
-	echo "<form action=\"editnote2.php\" method=\"post\">";
-	echo "Note:<br>";
-	echo "<textarea name=\"blognote\" rows=20 cols=100 style=\"resize:none\">";
-	echo $row[3];
-	echo "</textarea><br>";
-	echo "Keyword:<br>";
-	echo "<textarea name=\"keyword\" rows=1 cols=100 style=\"resize:none\">";
-	echo $row[4];
-	echo "</textarea><br>";
-	echo "<input type=\"submit\" value=\"send note\">";
-	echo "</form>";
-	echo "<form action=\"index.php\" method=\"post\">";
-	echo "<input type=\"submit\" value=\"cancel\">";
-	echo "</form>";
-?>
-</body>
+	<body>
+		<?php
+			$toedit=$_POST["notes"];
+			include 'secretpasswords.php';
+			try {
+				$pdo = new PDO("mysql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname, $user, $password);
+			}  catch (PDOException $e) {
+				echo "WRONG! PDO failed";
+				throw new Exception($e->getMessage());
+			}
+			$query=$pdo->prepare("SELECT * FROM blog where blog_id=?");
+			$query->execute([$toedit]);
+			$query->setFetchMode(PDO::FETCH_NUM);
+			$row = $query->fetch();
+			$_SESSION['id'] = $row[0];
+			echo "<form action=\"editnote2.php\" method=\"post\">";
+			echo "Note:<br>";
+			echo "<textarea name=\"blognote\" rows=20 cols=100 style=\"resize:none\">";
+			echo $row[3];
+			echo "</textarea><br>";
+			echo "Keyword:<br>";
+			echo "<textarea name=\"keyword\" rows=1 cols=100 style=\"resize:none\">";
+			echo $row[4];
+			echo "</textarea><br>";
+			echo "<input type=\"submit\" value=\"send note\">";
+			echo "</form>";
+			echo "<form action=\"index.php\" method=\"post\">";
+			echo "<input type=\"submit\" value=\"cancel\">";
+			echo "</form>";
+		?>
+	</body>
 </html>
