68aa375943ea8d0198af1143258c8c3c468e8635
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2022-09-04 15:49:09 +0200

    Reformat select_keyword.php

--- select_keyword.php
+++ select_keyword.php
@@ -1,17 +1,20 @@
 <?php
-header('location: index.php');
-session_start();
-include("placeforboilerplatecode.php");
-check_ssl();
+	header('location: index.php');
+	session_start();
+	include("placeforboilerplatecode.php");
+	check_ssl();
 
-$_SESSION["keyword"] = $_POST["keyword"];
-echo $_SESSION["keyword"];
-if (!strcmp($_SESSION["keyword"], "all")) {
-	$_SESSION["keyword"] = "";
-}
-$_SESSION["reset_pages"] = true;
+	$_SESSION["keyword"] = $_POST["keyword"];
+	echo $_SESSION["keyword"];
+
+	if (!strcmp($_SESSION["keyword"], "all")) {
+		$_SESSION["keyword"] = "";
+	}
+
+	$_SESSION["reset_pages"] = true;
 ?>
+
 <html>
-<body>
-</body>
+	<body>
+	</body>
 </html>
