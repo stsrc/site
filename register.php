9d58966cb9650d3f6a3bae2f735dc3d8f91cbe10
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2022-09-04 15:47:20 +0200

    Reformat register.php

--- register.php
+++ register.php
@@ -3,7 +3,11 @@
         check_ssl();
 	session_start();
 	include("simple-php-captcha.php");
-	$_SESSION['captcha'] = simple_php_captcha();
+	try {
+		$_SESSION['captcha'] = simple_php_captcha();
+	} catch (Exception $e) {
+		echo "simple_php_captcha() exception. " . $e->getMessage();
+	}
 ?>
 
 <html>
@@ -11,27 +15,27 @@
 		<link rel="stylesheet" href="register.css" type="text/css">
 	</head>
 	<body>
-	<div id="header">
-		<h1>Yet another fancy site</h1>
-		<hr>
-	</div>
-	<div id="container">
-		<div id="one">
-			<form action="register2.php" method="post">
-				Username:<br><input type="text" name="username">
-				<p style="font-size:12px"> 1-20 characters. Allowed characters: A-Z, a-z, 0-9, !, @, #, $, %, ^, &, *, (, ),_</p><br>
-				E-mail:<br><input type="text" name="email"><br>
-				Password:<br><input type="password" name="password">
-				<p style="font-size:12px"> 6-20 characters. Allowed characters: A-Z, a-z, 0-9, !, @, #, $, %, ^, &, *, (, ),_</p><br>
-				Retype password:<br><input type="password" name="repassword"><br><br>
-	<?php
-		echo '<img src="' . $_SESSION['captcha']['image_src'] . '" alt="CAPTCHA code"><br>';
-?>
-				Captcha:<br><input type="text" name="captcha"><br><br>
-				<input type="submit" value="Register">
-			</form>
+		<div id="header">
+			<h1>Yet another fancy site</h1>
+			<hr>
 		</div>
-	</div>
+		<div id="container">
+			<div id="one">
+				<form action="register2.php" method="post">
+					Username:<br><input type="text" name="username">
+					<p style="font-size:12px"> 1-20 characters. Allowed characters: A-Z, a-z, 0-9, !, @, #, $, %, ^, &, *, (, ),_</p><br>
+					E-mail:<br><input type="text" name="email"><br>
+					Password:<br><input type="password" name="password">
+					<p style="font-size:12px"> 6-20 characters. Allowed characters: A-Z, a-z, 0-9, !, @, #, $, %, ^, &, *, (, ),_</p><br>
+					Retype password:<br><input type="password" name="repassword"><br><br>
+					<?php
+						echo '<img src="' . $_SESSION['captcha']['image_src'] . '" alt="CAPTCHA code"><br>';
+					?>
+					Captcha:<br><input type="text" name="captcha"><br><br>
+					<input type="submit" value="Register">
+				</form>
+			</div>
+		</div>
 	</body>
 </html>
 
