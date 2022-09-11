3f967cecac62649a446bb7b33451c9af46c69f9b
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2022-09-11 18:19:56 +0200

    Add links to github/linkedin/mail.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import pic_github from './assets/github.svg';
+import pic_mail from './assets/mail.svg';
+import pic_linkedin from './assets/linkedin.svg';
 
 function App() {
   return (
     <div id="container">
       <div id="one">
-	Hello world.
+	Konrad Gotfryd site
+	<div id="svglinks">
+  	  <a href="https://github.com/stsrc"><img src={pic_github} alt="github" style={{
+	    width: '24px',
+	    height: '24px'
+	  }}/></a>
+  	  <a href="https://linkedin.com/in/konrad-gotfryd-4aa205136"><img src={pic_linkedin} alt="linkedin" style={{
+	    width: '24px',
+	    height: '24px'
+	  }}/></a>
+  	  <a href="mailto:kgotfryd.4l@gmail.com"><img src={pic_mail} alt="mail" style={{
+	    width: '24px',
+	    height: '24px'
+	  }}/></a>
+	</div>
       </div>
       <div id="two">
         Hello world.
